c890a0853450d88c8119c2296d753634054eb97c
Asher Feinstein <asher.feinstein@gmail.com>
2025-08-19 11:21:03 -0400

    Made headlights slightly rotate to the direction your facing

---
+++ Car/headlights.gd
@@ -0,0 +1,17 @@
+extends Node2D
+#turns headlights in direction car is turning
+@onready var leftHeadlight = $LightOccluder2D
+@onready var rightHeadlight = $LightOccluder2D2
+
+func _physics_process(delta: float) -> void:
+	var headlightTween = get_tree().create_tween()
+	headlightTween.set_parallel(true)
+	if Input.is_action_pressed("left"):
+		headlightTween.tween_property(leftHeadlight,"rotation",deg_to_rad(-10), .2)
+		headlightTween.tween_property(rightHeadlight,"rotation",deg_to_rad(170), .2)
+	elif Input.is_action_pressed("right"):
+		headlightTween.tween_property(leftHeadlight,"rotation",deg_to_rad(10), .2)
+		headlightTween.tween_property(rightHeadlight,"rotation",deg_to_rad(190), .2)
+	else:
+		headlightTween.tween_property(leftHeadlight,"rotation",0, .2)
+		headlightTween.tween_property(rightHeadlight,"rotation",deg_to_rad(180), .2)
--- bullets/bullets.gd
+++ bullets/bullets.gd
@@ -18,3 +18,7 @@ func _on_bullet_active_time_timeout() -> void:
 
 func _on_seen_in_timer_timeout() -> void:
 	visible = true
+
+
+func _on_detect_collision_body_entered(body: Node2D) -> void:
+	queue_free()
